0672cf1989026b330e5e1fa35abaea3cdcd1ecfd
Jorge Ramirez <jorgeramirezinstituto@gmail.com>
2022-06-10 12:47:24 +0200

    Darbuka - OnGoing

---
+++ main.js
@@ -0,0 +1,58 @@
+function PlaySound1() {
+    var audio1 = new Audio();
+    audio1.src = "./sounds/Darbuka_06_76_SP.mp3";
+    document.getElementById(audio1);
+    audio1.play();
+}
+function PlaySound2() {
+    var audio2 = new Audio();
+    audio2.src = "./sounds/Darbuka_18_76_SP.mp3";
+    document.getElementById(audio2);
+    audio2.play();
+}
+function PlaySound3() {
+    var audio3 = new Audio();
+    audio3.src = "./sounds/Darbuka_42_76_SP.mp3";
+    document.getElementById(audio3);
+    audio3.play();
+}
+function PlaySound4() {
+    var audio4 = new Audio();
+    audio4.src = "./sounds/Darbuka_48_76_SP.mp3";
+    document.getElementById(audio4);
+    audio4.play();
+}
+function PlaySound5() {
+    var audio5 = new Audio();
+    audio5.src = "./sounds/Darbuka_66_76_SP.mp3";
+    document.getElementById(audio5);
+    audio5.play();
+}
+function PlaySound6() {
+    var audio6 = new Audio();
+    audio6.src = "./sounds/Darbuka_105_76_SP.mp3";
+    document.getElementById(audio6);
+    audio6.play();
+}
+function PlaySound7() {
+    var audio7 = new Audio();
+    audio7.src = "./sounds/Darbuka_118_76_SP.mp3";
+    document.getElementById(audio7);
+    audio7.play();
+}
+function PlaySound8() {
+    var audio8 = new Audio();
+    audio8.src = "./sounds/Darbuka_130_76_SP.mp3";
+    document.getElementById(audio8);
+    audio8.play();
+}
+
+
+
+
+function PlaySound9() {
+    var audio9 = new Audio();
+    audio9.src = "./sounds/Darbuka_159_76_SP.mp3";
+    document.getElementById(audio9);
+    audio9.play();
+  }
